7f2f12482db0ca6f2f1b52fe42739bebb34b1290
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-06-07 13:17:57 +0530

    loadBreed Done

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-async function start(){
+async function start() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json();
+  breedNameList(data.message);
+}
 
-    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+start();
+
+function breedNameList(breedList) {
+  document.querySelector(".breed").innerHTML = `
+   <select onchange="loadBreed(this.value)">
+   <option>Choose a dog Breed</option>
+   ${Object.keys(breedList)
+     .map((breed) => {
+       return `"<option>${breed}</option>"`;
+     })
+     .join("")}
+   
+   
+   </select>
+   `;
+}
+
+async function loadBreed(breed) {
+  if (breed !="Choose a dog Breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
     const data = await response.json();
 
     console.log(data);
-
+  }
 }
-
-start()
