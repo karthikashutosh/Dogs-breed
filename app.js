3a709bdf1ef6bf42a6f29235ad8fe96bc32e02f0
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-06-07 18:47:30 +0530

    transition Done

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function breedNameList(breedList) {
 }
 
 async function loadBreed(breed) {
-  if (breed !="Choose a dog Breed") {
+  if (breed != "Choose a dog Breed") {
     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
     const data = await response.json();
 
@@ -30,9 +30,28 @@ async function loadBreed(breed) {
   }
 }
 
-function showImages(images){
-  document.querySelector("#slide").innerHTML=`
+function showImages(images) {
+  let currentPosition = 0;
+  document.querySelector("#slide").innerHTML = `
   <div class="slide" style="background-image: url('${images[0]}');">
   <div class="slide" style="background-image: url('${images[0]}');">
-  `
+  `;
+  currentPosition += 2;
+  setInterval(nextSlide, 3000);
+
+  function nextSlide() {
+    document.querySelector(".slideshow").insertAdjacentHTML(
+      "beforeend",
+      `  <div class="slide" style="background-image: url('${images[currentPosition]}');">
+    `
+    );
+    setTimeout(function () {
+      document.querySelector(".slide").remove();
+    }, 1000);
+    if (currentPosition + 1 > images.length) {
+      currentPosition = 0;
+    } else {
+      currentPosition++;
+    }
+  }
 }
